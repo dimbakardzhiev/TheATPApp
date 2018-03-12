bc4e6cde9fc5b427c2d2f7a33004b10ab60e97dc
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-03-12 09:43:56 +0000

    background added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
   resources :rankings
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
