721c7e38f759974ba24f9f4d75760b75a00d6310
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-03-14 21:27:46 +0000

    header

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "The ATP App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
