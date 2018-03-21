a72167270ba170fb2726e5773c07db83383d7ccc
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-03-21 17:38:13 +0000

    trying to fix heroku email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,10 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "AWAD_atp_#{Rails.env}"
+  config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'awad-atp.herokuapp.com'
+  host = 'https://awad-atp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
